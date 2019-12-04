14d18faead697dd39d77cb036153a83cb59fdb9c
Khwezi Flatela <qwezzy@gmail.com>
2019-12-04 09:37:00 +0200

    First Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+$(".jumbotron").css({ height: $(window).height() + "px" });
+
+$(window).on("resize", function() {
+  $(".jumbotron").css({ height: $(window).height() + "px" });
+});
